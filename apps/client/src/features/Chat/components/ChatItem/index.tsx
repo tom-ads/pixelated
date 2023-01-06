bcfb0313c689ff4b8cc7dbba58b84b0f7230c458
tom-ads <thomas_adams@hotmail.co.uk>
2023-01-06 14:00:39 +0000

    fix: chat item not wrapping

--- apps/client/src/features/Chat/components/ChatItem/index.tsx
+++ apps/client/src/features/Chat/components/ChatItem/index.tsx
@@ -4,12 +4,18 @@ type ChatItemProps = {
   value: Message
 }
 
+/* 
+  Use whitespace-pre-wrap to preserve newlines and spaces in message, while the
+  parent is flex'd w/ word break to wrap down onto the next line within the container
+*/
 export const ChatItem = ({ value }: ChatItemProps): JSX.Element => {
   return (
     <li className="space-y-[6px]">
       <p className="text-base">{value.sender}</p>
-      <div className="bg-purple-90 py-2 px-3 text-sm rounded-[4px] inline-block">
-        {value.message}
+      <div className="flex break-words">
+        <div className="bg-purple-90 py-2 px-3 text-sm rounded-[4px] whitespace-pre-wrap overflow-x-hidden">
+          {value.message}
+        </div>
       </div>
     </li>
   )
