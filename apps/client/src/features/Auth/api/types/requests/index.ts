7af874beff3b5adc0f80123baf3b49203b0a46ab
tom-ads <thomas_adams@hotmail.co.uk>
2023-01-13 20:43:22 +0000

    fix: failing build

--- apps/client/src/features/Auth/api/types/requests/index.ts
+++ apps/client/src/features/Auth/api/types/requests/index.ts
@@ -1,6 +1,4 @@
-import VerifyDetailsRequest from './verify_details'
-import VerifyOrganisationRequest from './verify_organisation'
 import RegisterRequest from './register'
 import LoginRequest from './login'
 
-export type { LoginRequest, VerifyDetailsRequest, VerifyOrganisationRequest, RegisterRequest }
+export type { LoginRequest, RegisterRequest }
